4ddabbbf3451fb4935f6e7e395241662791757e7
cihat <cihat02020202@gmail.com>
2021-05-10 16:54:46 +0300

    add custom text component

---
+++ src/components/CustomText.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: 'span'
+    },
+    size: {
+      type: String,
+      default: 'normal', //normal: 14, small: 12, xsmall:11, xxsmal: 10
+      validator: function (value) {
+      return [
+        'normal',
+        'small',
+        'xsmall',
+        'xxsmal'
+      ].indexOf(value) !== -1
+    }
+    }
+  }
+}
+</script>
+
+<template>
+  <component :is="tag" :class="size">
+    <slot />
+  </component>
+</template>
+
+<style lang="scss" scoped>
+  .small {
+    font-size: 12px;
+  }
+
+  .xsmall {
+    font-size: 11px;
+  }
+
+  .xxsmall {
+    font-size: 10px;
+  }
+</style>
