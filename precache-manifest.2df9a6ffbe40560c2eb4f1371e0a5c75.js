6e442ed0364ab199255a5e3fc4260465ada2ebe4
cihat <cihat02020202@gmail.com>
2021-05-14 01:28:02 +0300

    deploy

---
+++ precache-manifest.2df9a6ffbe40560c2eb4f1371e0a5c75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf0eb80cd21553f530a",
+    "url": "/instagram-clone/css/app.b5ff4f59.css"
+  },
+  {
+    "revision": "5f76ba50b648bfe15de3fc41f4b0aea1",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "46b2c483d2b2ee44057a",
+    "url": "/instagram-clone/js/ProfilePost.a07152db.js"
+  },
+  {
+    "revision": "caf0eb80cd21553f530a",
+    "url": "/instagram-clone/js/app.78048abd.js"
+  },
+  {
+    "revision": "07cf2d6e026a640e5808",
+    "url": "/instagram-clone/js/chunk-vendors.7e6c06a5.js"
+  },
+  {
+    "revision": "4ecab1637f7df7bbb0d3",
+    "url": "/instagram-clone/js/profile.7d3d95b1.js"
+  },
+  {
+    "revision": "de4553f89e770d76b917f50ebb2fbdb1",
+    "url": "/instagram-clone/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/instagram-clone/robots.txt"
+  }
+]);
